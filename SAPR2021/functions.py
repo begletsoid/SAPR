2b9cd3ec4a020d18c7ba38577424eb6b418abf03
begletsoid <vilkinw@gmail.com>
2021-10-30 20:18:18 +0300

    update

--- SAPR2021/functions.py
+++ SAPR2021/functions.py
@@ -1,1 +1,82 @@
+import numpy
+import matplotlib as ptl
+from numpy import linalg
+def generateReactionsMatrix(kernels, left, right):
+    count = len(kernels)
+    A = numpy.zeros((count+1, count+1), dtype=int).tolist()
+    for i in range(count):
+        A[i][i] += kernels[i].A*kernels[i].materialObj.elasticity/kernels[i].L
+        A[i][i+1] -= kernels[i].A*kernels[i].materialObj.elasticity/kernels[i].L
+        A[i+1][i] -= kernels[i].A*kernels[i].materialObj.elasticity/kernels[i].L
+        A[i+1][i+1] += kernels[i].A*kernels[i].materialObj.elasticity/kernels[i].L
+    if left == True:
+        A[0][0] = 1
+        A[1][0] = 0
+        A[0][1] = 0
+    if right == True:
+        A[count][count] = 1
+        A[count-1][count] = 0
+        A[count][count-1] = 0
+    print('Reaction Matrix', A)
+    return A
 
+def generateReactionsGlobalVector(kernels, concentrateds, left, right):
+    count = len(kernels)
+    knots = [0] * (count+1)
+    for conc in concentrateds:
+        knots[conc.point-1] = conc.power
+    print('knots', knots)
+    for kern in kernels:
+        print('running', kern.Q)
+    B = [0] * (count+1)
+    for i in range(count):
+        B[i] += knots[i]
+        if i != 0:
+            B[i] += kernels[i-1].Q * kernels[i-1].L/2
+        if i != count:
+            B[i] += kernels[i].Q * kernels[i].L/2
+    if left == True:
+        B[0] = 0
+    if right == True:
+        B[count] = 0
+    print('Reaction Global Vector', B)
+    return B
+
+def generateDeltas(kernels, concentrateds, left, right):
+    count = len(kernels)
+    A = generateReactionsMatrix(kernels, left, right)
+    B = generateReactionsGlobalVector(kernels, concentrateds, left, right)
+    A = linalg.inv(A)
+    ans = numpy.dot(A,B)
+    print('Deltas', ans)
+    return ans
+
+def solveN(kernels, concentrateds, left, right):
+    count = len(kernels)
+    A = generateReactionsMatrix(kernels, left, right)
+    B = generateReactionsGlobalVector(kernels, concentrateds, left, right)
+    A = linalg.inv(A)
+    v6 = numpy.dot(A,B)
+    N  = numpy.zeros((count, 2), dtype=int).tolist()
+    for i in range(count):
+        N[i][0] = (kernels[i].A * kernels[i].materialObj.elasticity/kernels[i].L) * (v6[i+1] -v6[i])
+        if kernels[i].Q != 0:
+            N[i][0] += (kernels[i].Q * kernels[i].L / 2)
+            N[i][1] -= kernels[i].Q #* kernels[i].L
+    print('N', N)
+    return N
+
+def solveU(kernels, concentrateds, left, right):
+    count = len(kernels)
+    A = generateReactionsMatrix(kernels, left, right)
+    B = generateReactionsGlobalVector(kernels, concentrateds, left, right)
+    A = linalg.inv(A)
+    v6 = numpy.dot(A,B)
+    U = numpy.zeros((count, 3), dtype=int).tolist()
+    for i in range(count):
+        U[i][0] = v6[i]
+        U[i][1] = (v6[i+1] - v6[i]) / kernels[i].L
+        U[i][1] += (kernels[i].Q * kernels[i].L) / (2 * kernels[i].materialObj.E * kernels[i].A)
+        U[i][2] = -(kernels[i].Q / (2*kernels[i].materialObj.E * kernels[i].A))
+    print('U', U)
+    return U
--- SAPR2021/models.py
+++ SAPR2021/models.py
@@ -1,27 +1,40 @@
-class kernel:
-    objects = []
-
-    def __init__(self, id, length=None, area=None, material = None):
-        if length == '':
-            length = 0
-        if material == '':
-            material = 0
-        if material == '':
-            material = 1
-        self.id = int(id)
-        self.length = float(length)
-        self.material = int(material)
-        self.area = float(area)
-        self.__class__.objects.append(self)
 
 class material:
     objects = []
     def __init__(self, id, elasticity, sigma):
-        self.id = id
+        self.id = int(id)
         self.elasticity = float(elasticity)
+        self.E = self.elasticity
         self.sigma = float(sigma)
         self.__class__.objects.append(self)
+        print('material', id)
 
+
+class kernel:
+    objects = []
+
+    def __init__(self, id, length=None, area=None, _material = None):
+        if length == '':
+            length = 0
+        if _material == '':
+            _material = 0
+        if _material == '':
+            _material = 1
+        self.id = int(id)
+        self.length = float(length)
+        self.L = self.length
+        self.material = int(_material)
+        self.materialObj = None 
+        for mat in material.objects:
+            if mat.id == self.material:
+                self.materialObj = mat
+        self.area = float(area)
+        self.A = self.area
+        self.__class__.objects.append(self)
+        self.Q = 0
+        for power in running.objects:
+            if power.kernel == self.id:
+                self.Q += power.power
 
 class concentrated:
     objects = []
